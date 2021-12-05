a2400f56798f32926770ea4fa7d7884cd8245525
Justin Yarbrough <80535881+j-yarbrough@users.noreply.github.com>
2021-12-05 12:45:53 -0700

    Update main.js
    captcha tweaks

--- docs/js/main.js
+++ docs/js/main.js
@@ -81,5 +81,7 @@ window.onload = function() {
     var el = document.getElementById('g-recaptcha-response'); 
     if (el) { 
       el.setAttribute('required', 'required'); 
+      el.setAttribute('tabindex','-1');
+      el.setAttribute('aria-hidden','true');
     } 
   }
