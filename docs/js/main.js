7129f8b91be96db55c06c0373bf312a7d5265a3f
Justin Yarbrough <80535881+j-yarbrough@users.noreply.github.com>
2021-12-28 16:51:10 -0700

    toc functionality changes
    adds functionality to set header text and list type in attribute for containing nav area.

--- docs/js/main.js
+++ docs/js/main.js
@@ -26,12 +26,19 @@ var toc = document.querySelectorAll('#toc'); //variable for TOC
 //fires script to build TOC if called for on page, otherwise ignores.
 
 if (toc.length > 0) {
-    toc[0].innerHTML = tocBuilder(toc[0].getAttribute('data-depth'));
+    toc[0].innerHTML = tocBuilder(
+        toc[0].getAttribute('data-depth'),
+        toc[0].getAttribute('aria-label'),
+        toc[0].getAttribute('data-list-type')
+    )
 };
-//function that builds TOC from h2s on page.
+//function that builds TOC from headings on page.
 
-function tocBuilder(depth) {
-    var builderOutput = '<h2 id="toc-header">On this Page</h2><ul>';
+function tocBuilder(depth,headerText, listType) {
+    //depth = how deep in the heading structure it should go. data-depth attribute, value between 2 and 6
+    //headerText: set by aria-label attribute, controls text for h2 at start of TOC
+    //list type: set by data-list-type attribute, should be 'ol' or 'ul'
+    var builderOutput = '<h2 id="toc-header">' + headerText + '</h2><' + listType + '>';
     var headingsToIndex;
     var tocContainer = document.querySelector('#toc');
     var headingIndex = { //index object
@@ -55,10 +62,10 @@ function tocBuilder(depth) {
     for (var i = 0; i < headingsToIndex.length; i++) { //checks if headings have id and tabindex, creates if they don't
         if (headingsToIndex[i].hasAttribute('tabindex') == false) {
             headingsToIndex[i].setAttribute('tabindex','-1');
-        }
+        };
         if (headingsToIndex[i].hasAttribute('id') == false) {
             headingsToIndex[i].setAttribute('id',headingsToIndex[i].textContent.replace(/[^A-Z0-9]/ig,'') + i);
-        }
+        };
     } //end loop
     for (var i = 0; i <headingsToIndex.length; i++) { //builds index object
 headingIndex.text[i] = headingsToIndex[i].textContent;
@@ -75,31 +82,30 @@ headingIndex.headingTag[i] = headingsToIndex[i].tagName.substr(1,1);
 switch (headingIndex.headingTag[i] - headingIndex.headingTag[i - 1]) { //determines list level relative to previous item, handles html appropriately
     case 0: builderOutput += '</li><li><a href="#' + headingIndex.idValue[i] + '">' + headingIndex.text[i] + '</a>';
     break;
-    case 1: builderOutput += '<ul><li><a href="#' + headingIndex.idValue[i] + '">' + headingIndex.text[i] + '</a>';
+    case 1: builderOutput += '<' + listType + '><li><a href="#' + headingIndex.idValue[i] + '">' + headingIndex.text[i] + '</a>';
     break;
-    case -1: builderOutput += '</li></ul></li><li><a href="#' + headingIndex.idValue[i] + '">' + headingIndex.text[i] + '</a>';
+    case -1: builderOutput += '</li></' + listType + '></li><li><a href="#' + headingIndex.idValue[i] + '">' + headingIndex.text[i] + '</a>';
     break;
-    case -2: builderOutput += '</li></ul></li></ul></li><li><a href="#' + headingIndex.idValue[i] + '">' + headingIndex.text[i] + '</a>';
+    case -2: builderOutput += '</li></' + listType + '></li></' + listType + '></li><li><a href="#' + headingIndex.idValue[i] + '">' + headingIndex.text[i] + '</a>';
     break;
-    case -3: builderOutput += '</li></ul></li></ul></li></ul></li><li><a href="#' + headingIndex.idValue[i] + '">' + headingIndex.text[i] + '</a>';
+    case -3: builderOutput += '</li></' + listType + '></li></' + listType + '></li></' + listType + '></li><li><a href="#' + headingIndex.idValue[i] + '">' + headingIndex.text[i] + '</a>';
     break;
-    case -4: builderOutput += '</li></ul></li></ul></li></ul></li></ul></li><li><a href="#' + headingIndex.idValue[i] + '">' + headingIndex.text[i] + '</a>';
+    case -4: builderOutput += '</li></' + listType + '></li></' + listType + '></li></' + listType + '></li></' + listType + '></li><li><a href="#' + headingIndex.idValue[i] + '">' + headingIndex.text[i] + '</a>';
     break;
     default: return; //cancels function if levels go deeper by more than 1
 } //end switch
     } //end loop
 switch (headingIndex.headingTag[i]) { //closes out the list or lists based on level of last heading
-    case 2: builderOutput += '</li></ul>';
+    case 2: builderOutput += '</li></' + listType + '>';
     break;
-    case 3: builderOutput += '</li></ul></li></ul>';
+    case 3: builderOutput += '</li></' + listType + '></li></' + listType + '>';
     break;
-    case 4: builderOutput += '</li></ul></li></ul></li></ul>';
+    case 4: builderOutput += '</li></' + listType + '></li></' + listType + '></li></' + listType + '>';
     break;
-    case 5: builderOutput += '</li></ul></li></ul></li></ul></li></ul>';
+    case 5: builderOutput += '</li></' + listType + '></li></' + listType + '></li></' + listType + '></li></' + listType + '>';
     break;
-    case 6: builderOutput += '</li></ul></li></ul></li></ul></li></ul></li></ul>';
+    case 6: builderOutput += '</li></' + listType + '></li></' + listType + '></li></' + listType + '></li></' + listType + '></li></' + listType + '>';
 } //end switch
-    tocContainer.setAttribute('aria-labelledby','toc-header');
     return builderOutput;
 } //end function
 //end toc
