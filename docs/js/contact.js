d6b0b756399ae4572f0e25ea00123dce575d764a
Justin Yarbrough <80535881+j-yarbrough@users.noreply.github.com>
2022-02-13 07:35:06 -0700

    Changes to contact form.

--- docs/js/contact.js
+++ docs/js/contact.js
@@ -1,77 +1,48 @@
-document.getElementById("contact-form").addEventListener("submit",function(evt)
-  {
-  
-  var response = grecaptcha.getResponse();
-  var canSubmit = true;
-  var errorCount = 0;
-  var robotError = document.getElementById('robot-error');
-  var nameError = document.getElementById('name-error');
-  var emailError = document.getElementById('email-error');
-  var nameField = document.getElementById('submitter-name');
-  var emailAddressField = document.getElementById('email-address');
-  var errorBlock = document.getElementById('error-block');
-  var errorIntro = document.getElementById('error-intro');
-  var messageLive = document.getElementById('message-live');
-  switch (response.length == 0) {
-      case true: errorCount++;
-robotError.hidden = false;
+var contactForm = document.getElementById('contact-form');
+var canSubmit;
+var firstError;
+
+contactForm.addEventListener('submit',validateForm);
+
+function validateForm() {
+    var nameField = document.querySelector('#submitter-name');
+    var nameError = document.querySelector('#name-error');
+    var emailField = document.querySelector('#email-address');
+    var emailError = document.querySelector('#email-error');
+    canSubmit = true;
+    switch (nameField.value.length > 0) {
+case true: fieldIsValid(nameField,nameError);
 break;
-case false: robotError.hidden = true;
+case false: fieldIsInvalid(nameField,nameError);
 break;
-  }
-  switch (nameField.value.length == 0) {
-      case true: errorCount++;
-      nameError.hidden = false;
-      nameField.setAttribute('aria-describedby','name-error');
-      break;
-      case false: nameError .hidden = true;
-      nameField.removeAttribute('aria-describedby');
-      break;
-  }
-  switch (emailAddressField.value.includes('@')) {
-      case false: errorCount++;
-      emailError.hidden = false;
-      emailAddressField.setAttribute('aria-describedby','email-error');
-      break;
-      case true: emailError.hidden = true;
-      emailAddressField.removeAttribute('aria-describedby');
-      break;
-  }
-  if (errorCount > 0) {
-      canSubmit = false;
-  }
-  switch (canSubmit) {
-      case true: break;
-      case false: evt.preventDefault();
-      errorBlock.hidden = false;
-      errorIntro.textContent = errorIntroTextGenerator(errorCount);
-      messageLive.textContent = errorLiveRegion(errorCount);
-      errorBlock.focus();
-      return false;
-      break;
-  }
-})
-function errorIntroTextGenerator(howManyErrors) {
-    var textOutput = '';
-    var magicWord = '';
-    if (howManyErrors == 1) {
-magicWord = 'error';
-    } else if (howManyErrors > 1) {
-        magicWord = 'errors';
-    };
-    textOutput = 'Please address the below ' + magicWord + ' and try submitting the form again.';
-    return textOutput;
-}
-function errorLiveRegion (howManyErrors) {
-    var textOutput = '';
-    var isPlural = ''
-    if (howManyErrors == 1) {
-textOutput = 'There was an error trying to submit the form.'
-isPlural = 'error';
-    } else if (howManyErrors > 1) {
-        textOutput = 'There were errors trying to submit the form.';
-        isPlural = 'errors';
-    }
-    textOutput += ' Please arrow down to review the ' + isPlural + ' and try submitting the form again.';
-    return textOutput;
-}
+    } //end switch
+    switch (emailField.value.includes('@')) {
+case true: fieldIsValid(emailField,emailError);
+break;
+case false: fieldIsInvalid(emailField,emailError);
+break;
+    } //end switch
+    switch (canSubmit) {
+        case true: break;
+        case false: event.preventDefault();
+        firstError.focus();
+        break;
+    } //end switch
+    return;
+}//end function
+
+function fieldIsValid(fieldVar, errorVar) {
+    fieldVar.removeAttribute('aria-describedby');
+    fieldVar.removeAttribute('aria-invalid');
+    errorVar.setAttribute('hidden','hidden');
+} //end function
+
+function fieldIsInvalid(fieldVar, errorVar) {
+    if (canSubmit == true) {
+canSubmit = false;
+firstError = fieldVar;
+    } //end if
+    errorVar.removeAttribute('hidden');
+    fieldVar.setAttribute('aria-describedby',errorVar.getAttribute('id'));
+    fieldVar.setAttribute('aria-invalid','true');
+} //end function
