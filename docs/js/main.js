ef1bbbe909ed6b900c7781bba39381bf380bd08a
Justin Yarbrough <80535881+j-yarbrough@users.noreply.github.com>
2022-07-10 17:34:50 -0700

    Changed functionality for script loader
    Script loader has been revised. Now runs off a for loop based off selectors and filenames stored in an array instead of each being its own if statement.

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,43 +2,38 @@
 // Will load scripts only for components that are being used on pages.
 // This script will check for certain selectors and append script tags to the end of the body element.
 
-var bodyElement = document.querySelector('body');
 var pageURL = window.location.href;
-var scriptsToAdd = '';
 var rootCheckerOutput = 'js/';
+
+// Array for checks to be done
+// Format for each check is [selector, filename]
+// Note: Filename should not contain file extension
+
+var indexOfChecks = [
+    ['#nav-btn','nav-collapse'],
+    ['#toc','toc'],
+    ['.accordion-button','accordion'],
+    ['#contact-form','contact']
+    ]; //end index
+
 // checks URL to see if it's a root or approved subfolder
 
 if (pageURL.includes('alt-text')) {
     rootCheckerOutput = '../js/';
 };
 
-// checks for nav collapse
+// Loop that does checks, runs function if check is true
 
-if (document.querySelectorAll('#nav-btn').length > 0) {
-    createScriptElements('nav-collapse');}
-
-//checks for TOC
-
-if (document.querySelectorAll('#toc').length > 0) {
-    createScriptElements('toc');
-};
-
-//checks for accordions
-
-if (document.querySelectorAll('.accordion-button').length > 0) {
-    createScriptElements('accordion');
-}
-
-// checks for contact form validation
-
-if (document.querySelectorAll('#contact-form').length > 0) {
-    createScriptElements('contact');
-};
+for (var i = 0; i < indexOfChecks.length; i++) {
+    if (document.querySelectorAll(indexOfChecks[i][0]).length >0) {
+        createScriptElements(indexOfChecks[i][1]);
+    }
+} //end loop
 
 //function that creates elements
 
 function createScriptElements (scriptFileName) {
     var elementBeingCreated = document.createElement('script');
     elementBeingCreated.setAttribute('src',rootCheckerOutput + scriptFileName + '.js');
-bodyElement.appendChild(elementBeingCreated);
+document.body.appendChild(elementBeingCreated);
 }
