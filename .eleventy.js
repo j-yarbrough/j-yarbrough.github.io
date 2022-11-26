62fcfe57e744b929d32efb4d5a98c4c2c2e76c4f
Justin Yarbrough <80535881+j-yarbrough@users.noreply.github.com>
2022-11-26 10:59:27 -0700

    Converted TOC to accordion, styled to float right on desktop, fixed issue with alt text post.

--- .eleventy.js
+++ .eleventy.js
@@ -32,9 +32,9 @@ module.exports = (function(eleventyConfig) {
         return `<h${level} class="accordion-header" id="${accordionId}" tabindex="-1"><button class="accordion-button" aria-expanded="false" id="${accordionId}-button"><span class="accordion-indicator" aria-hidden="true">&plus;</span>
         <span id="${accordionId}-label">${label}</span>
         </button></h${level}>
-<section aria-labelledby="${accordionId}-label" class="accordion-panel" id="${accordionId}-panel">
+<div class="accordion-panel" id="${accordionId}-panel">
 ${content}
-</section>`
+</div>`
     });
     eleventyConfig.addPairedShortcode("formcontainer", function(content, id, name, method, action) {
         return `<form id="${id}" name="${name}" method="${method}" action="${action}">${content}</form>`
