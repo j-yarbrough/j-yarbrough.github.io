81082208b73c2fb464727b7f159a0c6ed3c2beb0
j-yarbrough <j-yarbrough@users.noreply.github.com>
2023-11-25 18:41:18 +0000

    deploy: 13219fb4ff0782ec53806a34e155cec3c4e62666

---
+++ js/accgroup.js
@@ -0,0 +1,29 @@
+// used to control a group of accordions
+
+var accordionGroupButtons = document.querySelectorAll('.acc-group-btn');
+accordionGroupButtons.forEach((accordionGroupButtons) => {
+    accordionGroupButtons.addEventListener('click',accordionGroupAction);
+});
+
+function accordionGroupAction () {
+    var accordionGroupContainer = this.parentElement.parentElement;
+    var accordionGroupAccButtons = accordionGroupContainer.querySelectorAll('.accordion-button');
+    var collapseOrExpand = this.getAttribute('data-coe');
+    var ariaExpandedValue;
+    // determines if loop later will be looking for expanded or collapsed accordions
+    // false means we're expanding all
+    // true means we're collapsing all
+    switch (collapseOrExpand) {
+        case 'c': ariaExpandedValue = 'true'; break;
+        case 'e': ariaExpandedValue = 'false'; break;
+    }
+    accordionGroupAccButtons.forEach((accordionGroupAccButtons) => {
+if((accordionGroupAccButtons.getAttribute('aria-expanded')) == ariaExpandedValue) {
+    accordionGroupAccButtons.click();
+}
+    });
+    switch(collapseOrExpand) {
+        case 'c': ariaLiveHandler('All accordions in group collapsed.'); break;
+        case 'e': ariaLiveHandler('All accordions in group expanded.'); break;
+    }
+}
---
+++ js/arialive.js
@@ -0,0 +1,8 @@
+// Handles all aria live announcements
+// called on by multiple scripts
+
+function ariaLiveHandler (messageToAnnounce) {
+    var ariaRegion = document.querySelector("#aria-live-region");
+    ariaRegion.textContent = '';
+    setTimeout(ariaRegion.textContent = messageToAnnounce, 200);
+}
