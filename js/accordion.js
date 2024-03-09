8d7428eddea09d45598a7883fc3c3ab363da4741
Justin Yarbrough <80535881+j-yarbrough@users.noreply.github.com>
2024-03-09 12:23:49 -0600

    All the changes

--- js/accordion.js
+++ js/accordion.js
@@ -18,4 +18,32 @@ function triggerAccordion() {
     };
     accordionPanel.classList.toggle('accordion-panel-show')
     accordionPanel.classList.toggle('accordion-panel-hide')
+    if (this.hasAttribute('data-accgroup')) {accGroupTogglerChecker(this)}
+}
+
+function accGroupTogglerChecker(buttonClicked) {
+    var GroupContainerIDValue = buttonClicked.getAttribute('data-accgroup');
+    var groupContainerThingy = document.querySelector('#' + GroupContainerIDValue);
+    var groupContainerThingyAccButtons = groupContainerThingy.querySelectorAll('.accordion-button');
+    var collapsedAccCount = 0;
+    var expandedAccCount = 0;
+    var expandControl = groupContainerThingy.querySelector('.acc-group-expand-btn');
+    var collapseControl = groupContainerThingy.querySelector('.acc-group-collapse-btn');
+    groupContainerThingyAccButtons.forEach((groupContainerThingyAccButtons) => {
+        if ((groupContainerThingyAccButtons.getAttribute('aria-expanded')) == 'true') {
+            expandedAccCount++;
+        } else {
+            collapsedAccCount++;
+        }
+    });
+    if(expandedAccCount == 0) {
+        expandControl.setAttribute('aria-pressed','false');
+        collapseControl.setAttribute('aria-pressed','true');
+    } else if (collapsedAccCount== 0) {
+        collapseControl.setAttribute('aria-pressed','false');
+        expandControl.setAttribute('aria-pressed','true');
+    } else {
+        expandControl.setAttribute('aria-pressed','mixed');
+        collapseControl.setAttribute('aria-pressed','mixed');
+    }
 }
