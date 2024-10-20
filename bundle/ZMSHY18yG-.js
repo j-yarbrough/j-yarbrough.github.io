c19d6ae2172cfaa7c006153559f7451d5d9b3779
j-yarbrough <j-yarbrough@users.noreply.github.com>
2024-10-20 16:00:40 +0000

    deploy: 990f432a02638a79a68437146f8ef6cc3ce985b2

--- bundle/ZMSHY18yG-.js
+++ bundle/ZMSHY18yG-.js
@@ -1,3 +1,11 @@
+// Handles all aria live announcements
+// called on by multiple scripts
+
+function ariaLiveHandler (messageToAnnounce) {
+    var ariaRegion = document.querySelector("#aria-live-region");
+    ariaRegion.textContent = '';
+    setTimeout(ariaRegion.textContent = messageToAnnounce, 200);
+}
 // script to handle all contact form validation actions
 
 //global variables
--- js/arialive.js
+++
@@ -1,8 +0,0 @@
-// Handles all aria live announcements
-// called on by multiple scripts
-
-function ariaLiveHandler (messageToAnnounce) {
-    var ariaRegion = document.querySelector("#aria-live-region");
-    ariaRegion.textContent = '';
-    setTimeout(ariaRegion.textContent = messageToAnnounce, 200);
-}
