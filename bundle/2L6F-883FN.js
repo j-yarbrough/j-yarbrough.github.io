45eb3225b7996f40aed7f232ce2903a14031dff5
j-yarbrough <j-yarbrough@users.noreply.github.com>
2024-11-10 16:05:16 +0000

    deploy: 77adebb54ac84cba43e171ed17b7885b418cff6c

---
+++ bundle/2L6F-883FN.js
@@ -0,0 +1,24 @@
+// Script for accordions
+
+var accordionButtons = document.querySelectorAll('.accordion-button');
+accordionButtons.forEach((accordionButtons) => {
+    accordionButtons.addEventListener('click',triggerAccordion)
+});
+
+function triggerAccordion() {
+    var accordionContainer = this.parentElement.parentElement;
+    var accordionHeading = accordionContainer.querySelector('.accordion-header');
+    var accordionPanel = accordionContainer.querySelector('.accordion-panel');
+    var accordionIndicator = accordionContainer.querySelector('.accordion-indicator');
+    switch (this.ariaExpanded) {
+        case 'true': this.ariaExpanded = false;
+        accordionIndicator.innerHTML = '&rarr;';
+        accordionHeading.removeAttribute('data-show');
+        break;
+        case 'false': this.ariaExpanded = true;
+        accordionIndicator.innerHTML = '&darr;';
+        accordionHeading.setAttribute('data-show','show');
+        break;
+    };
+    accordionPanel.classList.toggle('accordion-panel-show')
+}
---
+++ bundle/yzZvYR4waE.js
@@ -0,0 +1,96 @@
+// script to handle all contact form validation actions
+
+//global variables
+
+var formContainer = document.querySelector('#contact-form');
+var fieldsToValidate = document.querySelectorAll('input[aria-required], textarea[aria-required]');
+
+//event listeners
+
+formContainer.addEventListener('submit',submitForm);
+formContainer.addEventListener('reset',resetForm);
+
+//functions
+
+//function to perform validation
+
+function validateField (fieldToValidate) {
+    var isValid;
+    if (fieldToValidate.getAttribute('autocomplete') == 'email') {
+        switch ((fieldToValidate.value.length >= 6) && (fieldToValidate.value.includes('@')) && (fieldToValidate.value.includes('.')) && (fieldToValidate.value.includes(' ') == false)) {
+            case true: isValid = true; break;
+            case false: isValid = false; break;
+        }
+    } else {
+        switch (fieldToValidate.value != '') {
+            case true: isValid = true; break;
+            case false: isValid = false; break;
+        }
+    }
+    applyValidationState(fieldToValidate, isValid);
+    return isValid;
+}
+
+function submitForm() {
+    var firstErrorField = undefined;
+    var errorCount = 0;
+    var errorCountString;
+    fieldsToValidate.forEach((fieldsToValidate) => {
+        switch (validateField(fieldsToValidate)) {
+            case true: break;
+            case false: if (errorCount == 0) {
+                firstErrorField = fieldsToValidate;
+            }
+            errorCount++
+            break;
+        }
+    });
+    switch (errorCount == 0) {
+        case true: break;
+        case false: event.preventDefault();
+        switch (errorCount == 1) {
+            case true: errorCountString = 'error';
+            break;
+            case false: errorCountString = `${errorCount} errors`;
+            break;
+        }
+        firstErrorField.focus();
+        ariaLiveHandler(`Please fix the ${errorCountString} with this form and try submitting again.`);
+    }
+}
+
+function applyValidationState (field, validOrNot) {
+    var fieldID = field.getAttribute('id');
+    switch (validOrNot) {
+        case true: field.removeAttribute('aria-invalid');
+        if (document.querySelector(`#${fieldID}-helper`)) {
+            field.setAttribute('aria-describedby',`${fieldID}-helper`)
+        } else {
+            field.removeAttribute('aria-describedby');
+        }
+        break;
+        case false: field.setAttribute('aria-invalid','true');
+        if (document.querySelector(`#${fieldID}-helper`)) {
+            field.setAttribute('aria-describedby',`${fieldID}-error ${fieldID}-helper`);
+        } else {
+            field.setAttribute('aria-describedby',`${fieldID}-error`);
+        }
+        break;
+    }
+}
+
+function resetForm() {
+    fieldsToValidate.forEach((fieldsToValidate) => {
+        applyValidationState(fieldsToValidate, true);
+    });
+    fieldsToValidate[0].focus();
+    ariaLiveHandler('All fields cleared');
+}
+// Handles all aria live announcements
+// called on by multiple scripts
+
+function ariaLiveHandler (messageToAnnounce) {
+    var ariaRegion = document.querySelector("#aria-live-region");
+    ariaRegion.textContent = '';
+    setTimeout(ariaRegion.textContent = messageToAnnounce, 200);
+}
