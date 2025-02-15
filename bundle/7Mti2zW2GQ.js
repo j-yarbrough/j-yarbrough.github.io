09f5bc71bb2c3b741d036823b17cf39a0e6536e1
j-yarbrough <j-yarbrough@users.noreply.github.com>
2025-02-15 17:52:03 +0000

    deploy: 9f4295330e2907cdc81f3c9471370514a647f63e

--- bundle/7Mti2zW2GQ.js
+++ bundle/7Mti2zW2GQ.js
@@ -75,16 +75,12 @@ function applyValidationState (field, validOrNot) {
 }
 // Script for accordions
 
-var accordionButtons = document.querySelectorAll('#nav-btn, .accordion-button');
-accordionButtons.forEach((accordionButtons) => {
+document.querySelectorAll('#nav-btn, .accordion-button').forEach((accordionButtons) => {
     accordionButtons.addEventListener('click',triggerAccordion)
 });
 
 function triggerAccordion() {
-    var accordionContainer = this.parentElement.parentElement;
-    var accordionHeading = accordionContainer.querySelector('.accordion-header');
-    var accordionPanel = document.querySelector('#' + this.getAttribute('aria-controls'));
-    var accordionIndicator = accordionContainer.querySelector('.accordion-indicator');
+    var accordionIndicator = this.querySelector('.accordion-indicator');
     switch (this.ariaExpanded) {
         case 'true': this.ariaExpanded = false;
         accordionIndicator.innerHTML = '&rarr;';
@@ -93,11 +89,6 @@ function triggerAccordion() {
         accordionIndicator.innerHTML = '&darr;';
         break;
     };
-    if (this.id == 'nav-btn') {
-        accordionPanel.classList.toggle('nav-links-show')
-    } else {
-        accordionPanel.classList.toggle('accordion-panel-show')
-    }
 }
 // Handles all aria live announcements
 // called on by multiple scripts
--- bundle/mpPOZK6b7T.js
+++ bundle/mpPOZK6b7T.js
@@ -19,16 +19,12 @@ var listElement = document.createElement('ol');
 }
 // Script for accordions
 
-var accordionButtons = document.querySelectorAll('#nav-btn, .accordion-button');
-accordionButtons.forEach((accordionButtons) => {
+document.querySelectorAll('#nav-btn, .accordion-button').forEach((accordionButtons) => {
     accordionButtons.addEventListener('click',triggerAccordion)
 });
 
 function triggerAccordion() {
-    var accordionContainer = this.parentElement.parentElement;
-    var accordionHeading = accordionContainer.querySelector('.accordion-header');
-    var accordionPanel = document.querySelector('#' + this.getAttribute('aria-controls'));
-    var accordionIndicator = accordionContainer.querySelector('.accordion-indicator');
+    var accordionIndicator = this.querySelector('.accordion-indicator');
     switch (this.ariaExpanded) {
         case 'true': this.ariaExpanded = false;
         accordionIndicator.innerHTML = '&rarr;';
@@ -37,9 +33,4 @@ function triggerAccordion() {
         accordionIndicator.innerHTML = '&darr;';
         break;
     };
-    if (this.id == 'nav-btn') {
-        accordionPanel.classList.toggle('nav-links-show')
-    } else {
-        accordionPanel.classList.toggle('accordion-panel-show')
-    }
 }
