c4a848f7aa37bdde65b939d536fe12a8d2be4387
j-yarbrough <j-yarbrough@users.noreply.github.com>
2025-03-29 20:28:24 +0000

    deploy: 2430c0892705b205b96d12fafc883327ff5f973b

--- bundle/HarpMhvroi.js
+++ bundle/HarpMhvroi.js
@@ -17,6 +17,23 @@ var listElement = document.createElement('ol');
     });
     tocContainer .appendChild(listElement);
 }
+//dialog event listeners
+
+document.querySelectorAll('.modal-trigger').forEach((dialogTrigger) => {
+    dialogTrigger.addEventListener('click', triggerDialog)
+});
+document.querySelectorAll('.modal-close').forEach((dialogCloseBtn) => {
+    dialogCloseBtn.addEventListener('click',closeDialog);
+});
+        
+        //functions
+
+function triggerDialog() {
+this.parentElement.nextElementSibling.showModal();
+}
+function closeDialog() {
+    this.parentElement.parentElement.close();
+}
 // Script for accordions
 
 document.querySelectorAll('#nav-btn, .accordion-button').forEach((accordionButtons) => {
