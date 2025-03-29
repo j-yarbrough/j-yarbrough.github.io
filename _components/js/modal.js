d23343620943947c66d3a776a205f572612af835
Justin Yarbrough <80535881+j-yarbrough@users.noreply.github.com>
2025-03-29 13:17:25 -0500

    some work

---
+++ _components/js/modal.js
@@ -0,0 +1,17 @@
+//dialog event listeners
+
+document.querySelectorAll('.modal-trigger').forEach((dialogTrigger) => {
+    dialogTrigger.addEventListener('click', triggerDialog)
+});
+document.querySelectorAll('.modal-close').forEach((dialogCloseButton) => {
+    dialogCloseButton.addEventListener('click',dialogClose)
+});
+//functions
+
+function triggerDialog() {
+
+}
+
+function closeDialog() {
+
+}
