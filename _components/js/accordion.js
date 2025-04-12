6782106f104e80b9a6414b72f9696bf909235787
Justin Yarbrough <80535881+j-yarbrough@users.noreply.github.com>
2025-04-12 12:50:29 -0500

    Revert "final fix hopefully"
    This reverts commit 5d8329ae5d65550caa529fbd5860b28d7c0032e5.

--- _components/js/accordion.js
+++ _components/js/accordion.js
@@ -1,15 +1,15 @@
 // event listeners
 
-document.querySelectorAll('.acc-button').forEach((accButton) => {
-    accButton.addEventListener('click',triggerAccordion);
+document.querySelectorAll('.accordion-button').forEach((button) => {
+    button.addEventListener('click',triggerAccordion);
 })
 
 // function
 
 function triggerAccordion () {
-    var accContainer = this.closest('jy-acc');
-    var indicator = accContainer.querySelector('.acc-indicator');
-    var panel = accContainer.querySelector('.acc-panel');
+    var accordionContainer = this.closest('jy-acc');
+    var indicator = accordionContainer.querySelector('.accordion-indicator');
+    var panel = accordionContainer.querySelector('.accordion-panel');
     if (this.getAttribute('aria-expanded') == 'true') {
         this.setAttribute('aria-expanded','false');
         indicator.innerHTML = '&rarr;';
@@ -17,6 +17,5 @@ function triggerAccordion () {
         this.setAttribute('aria-expanded','true');
         indicator.innerHTML = '&darr;';
     }
-    panel.classList.toggle('acc-panel-show');
-    panel.classList.toggle('acc-panel-hide');
+    panel.classList.toggle('accordion-panel-show');
 }
