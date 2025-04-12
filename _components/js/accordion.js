0d61e5e2c1f3c71fadb5cf74be23ad78271b606e
Justin Yarbrough <80535881+j-yarbrough@users.noreply.github.com>
2025-04-12 12:52:00 -0500

    Revert "fix for accordion chunked from nav scripting"
    This reverts commit 719e0f131a6d05e998584a5b6e1cbb189cbfecbf.

--- _components/js/accordion.js
+++ _components/js/accordion.js
@@ -1,21 +1,17 @@
-// event listeners
+// Script for accordions
 
-document.querySelectorAll('.accordion-button').forEach((button) => {
-    button.addEventListener('click',triggerAccordion);
-})
+document.querySelectorAll('#nav-btn, .accordion-button').forEach((accordionButtons) => {
+    accordionButtons.addEventListener('click',triggerAccordion)
+});
 
-// function
-
-function triggerAccordion () {
-    var accordionContainer = this.closest('jy-acc');
-    var indicator = accordionContainer.querySelector('.accordion-indicator');
-    var panel = accordionContainer.querySelector('.accordion-panel');
-    if (this.getAttribute('aria-expanded') == 'true') {
-        this.setAttribute('aria-expanded','false');
-        indicator.innerHTML = '&rarr;';
-    } else {
-        this.setAttribute('aria-expanded','true');
-        indicator.innerHTML = '&darr;';
-    }
-    panel.classList.toggle('accordion-panel-show');
+function triggerAccordion() {
+    var accordionIndicator = this.querySelector('.accordion-indicator');
+    switch (this.ariaExpanded) {
+        case 'true': this.ariaExpanded = false;
+        accordionIndicator.innerHTML = '&rarr;';
+        break;
+        case 'false': this.ariaExpanded = true;
+        accordionIndicator.innerHTML = '&darr;';
+        break;
+    };
 }
