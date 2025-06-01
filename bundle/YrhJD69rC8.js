1958b2dab0426058df2617da1ec2443087683178
j-yarbrough <j-yarbrough@users.noreply.github.com>
2025-06-01 18:32:03 +0000

    deploy: 5732455ffb1aadfd5617a1dc85baeaafb8d05ec8

--- bundle/YrhJD69rC8.js
+++ bundle/YrhJD69rC8.js
@@ -77,7 +77,7 @@ function applyValidationState (field, validOrNot) {
 // called on by multiple scripts
 
 function ariaLiveHandler (messageToAnnounce) {
-    var ariaRegion = document.querySelector("#aria-live-region");
+    var ariaRegion = document.querySelector('aria-live');
     ariaRegion.textContent = '';
     setTimeout(ariaRegion.textContent = messageToAnnounce, 200);
 }
