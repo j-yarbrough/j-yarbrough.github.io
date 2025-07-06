c7228724ba1c2c00fb2d7a17b7b96129c009ca34
Justin Yarbrough <80535881+j-yarbrough@users.noreply.github.com>
2025-07-06 12:41:58 -0500

    some script refactoring

--- _components/js/accordion.js
+++ _components/js/accordion.js
@@ -1,9 +1,6 @@
-// Script for accordions
-
-document.querySelectorAll('#nav-btn, .accordion-button').forEach((accordionButtons) => {
+document.querySelectorAll('.accordion-button').forEach((accordionButtons) => {
     accordionButtons.addEventListener('click',triggerAccordion)
 });
-
 function triggerAccordion()  {
     switch (this.ariaExpanded == 'true') {
         case true: this.ariaExpanded = 'false'; break;
