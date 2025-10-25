11cbda0a482228cff81edb7f71bd9f3e72862af9
j-yarbrough <j-yarbrough@users.noreply.github.com>
2025-10-25 20:11:57 +0000

    deploy: 27153655737044567b40fb2f7126ed69772ff5f5

---
+++ bundle/W_BFNbt4rQ.js
@@ -0,0 +1,49 @@
+window.addEventListener('load',function(){
+    document.querySelectorAll('form-wrapper').forEach((formWrapper) => {
+        formWrapper.firstElementChild.addEventListener('submit',validateForm)
+    })
+})
+
+function validateForm() {
+    event.preventDefault();
+    var fieldsToValidate = this.querySelectorAll('[aria-required]');
+    var isValid = true;
+    var firstErrorField = undefined;
+    fieldsToValidate.forEach((fieldToValidate) => {
+ if (validateField(fieldToValidate) == false) {
+    if (isValid) {
+        isValid = false;
+        firstErrorField= fieldToValidate;
+    }
+ }
+    })
+    if (isValid == false) {
+        event.preventDefault();
+        firstErrorField.focus();
+    } else {event.preventDefault(); alert('Field validates');}//only for testing purposes, will be commented out in prod
+}
+
+function validateField(valThisField) {
+var fieldContainer =     valThisField.parentElement;
+var fieldLabel=fieldContainer.querySelector('label').firstChild.textContent;
+var errorContainer = fieldContainer.querySelector('.validation-feedback');
+var fieldValue = valThisField.value.trim();
+var isFieldValid = true;
+valThisField.removeAttribute('aria-invalid');
+errorContainer.textContent = '';
+if (fieldValue.length == 0){
+    invalidateField(valThisField,fieldLabel,errorContainer,'cannot be blank');
+    isFieldValid = false;
+} else if (valThisField.getAttribute('inputmode') == 'email') {
+    if ((fieldValue.length < 6)&&(fieldValue.includes('.') ==false)&&(fieldValue.includes('@') == false)){
+        invalidateField(valThisField,fieldLabel,errorContainer,'is not valid. Example: name@example.com');
+        isFieldValid = false
+    }
+}
+return isFieldValid;
+}
+
+function invalidateField (field, label, errorContainer, errorText) {
+    field.setAttribute('aria-invalid','true');
+    errorContainer.textContent = `${label} ${errorText}.`;
+}
