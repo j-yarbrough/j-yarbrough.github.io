e04f79e77ec1f595e43d5fc3d50e7e75e20a3d4f
j-yarbrough <j-yarbrough@users.noreply.github.com>
2025-10-26 01:19:01 +0000

    deploy: cdb9d5a2dce85da668a599d7e0883dc959a201c8

--- bundle/PLSYlXseYk.js
+++ bundle/PLSYlXseYk.js
@@ -5,10 +5,14 @@ window.addEventListener('load',function(){
 })
 
 function validateForm() {
-    var fieldsToValidate = this.querySelectorAll('[aria-required]');
     var isValid = true;
     var firstErrorField = undefined;
-    fieldsToValidate.forEach((fieldToValidate) => {
+    var runningOnLocal = false; //stores if running locally, changes some form behaviors for testing
+    if (window.location.hostname.toLowerCase().includes('localhost')) {
+        runningOnLocal = true;
+        event.preventDefault();
+    }
+    this.querySelectorAll('[aria-required]').forEach((fieldToValidate) => {
  if (validateField(fieldToValidate) == false) {
     if (isValid) {
         isValid = false;
@@ -19,29 +23,31 @@ function validateForm() {
     if (isValid == false) {
         event.preventDefault();
         firstErrorField.focus();
+    } else if (runningOnLocal) {
+        alert('Form validated and would have submitted');
     }
 }
 function validateField(valThisField) {
 var fieldContainer =     valThisField.parentElement;
-var fieldLabel=fieldContainer.querySelector('label').firstChild.textContent;
+var label=fieldContainer.querySelector('label').firstChild.textContent;
 var errorContainer = fieldContainer.querySelector('.validation-feedback');
 var fieldValue = valThisField.value.trim();
-var isFieldValid = true;
 valThisField.removeAttribute('aria-invalid');
 errorContainer.textContent = '';
 if (fieldValue.length == 0){
-    invalidateField(valThisField,fieldLabel,errorContainer,'cannot be blank');
-    isFieldValid = false;
+    invalidateField(valThisField,errorContainer,`${label} cannot be blank.`);
+    return false;
 } else if (valThisField.getAttribute('inputmode') == 'email') {
-    if ((fieldValue.length < 6)&&(fieldValue.includes('.') ==false)&&(fieldValue.includes('@') == false)){
-        invalidateField(valThisField,fieldLabel,errorContainer,'is not valid. Example: name@example.com');
-        isFieldValid = false
+    if ((fieldValue.length < 6) || (fieldValue.includes('.') == false) || (fieldValue.includes('@') == false)){
+        invalidateField(valThisField,errorContainer,`${label} is not valid. Example: name@example.com.`);
+        return false;
     }
+} else {
+    return true;
 }
-return isFieldValid;
 }
 
-function invalidateField (field, label, errorContainer, errorText) {
+function invalidateField (field, errorContainer, errorText) {
     field.setAttribute('aria-invalid','true');
-    errorContainer.textContent = `${label} ${errorText}.`;
+    errorContainer.textContent = errorText;
 }
