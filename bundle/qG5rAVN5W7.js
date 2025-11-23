764bd81e73341e330d154d212c957f977ee438bd
j-yarbrough <j-yarbrough@users.noreply.github.com>
2025-11-23 16:15:55 +0000

    deploy: 7e6dfd6ae3e7295236d0416ecc1a089eb02b9bec

--- bundle/qG5rAVN5W7.js
+++ bundle/qG5rAVN5W7.js
@@ -8,12 +8,13 @@ function validateForm() {
     var isValid = true;
     var firstErrorField = undefined;
     var runningOnLocal = false; //stores if running locally, changes some form behaviors for testing
+    var subjectField = this.querySelector('input[name="subject"]');
     if (window.location.hostname.toLowerCase().includes('localhost')) {
         runningOnLocal = true;
         event.preventDefault();
     }
     this.querySelectorAll('[aria-required]').forEach((fieldToValidate) => {
- if (validateField(fieldToValidate) == false) {
+ if (!validateField(fieldToValidate)) {
     if (isValid) {
         isValid = false;
         firstErrorField= fieldToValidate;
@@ -23,8 +24,9 @@ function validateForm() {
     if (!isValid) {
         event.preventDefault();
         firstErrorField.focus();
-    } else if (runningOnLocal) {
-        alert('Form validated and would have submitted');
+    } else {
+        subjectField.value = '[yarbrough.info contact form] ' + subjectField.value;
+        if (runningOnLocal) {alert('Form is valid and would have submitted.');}
     }
 }
 function validateField(valThisField) {
@@ -38,7 +40,9 @@ if (fieldValue.length == 0){
     invalidateField(valThisField,errorContainer,`${label} cannot be blank.`);
     return false;
 } else if (valThisField.getAttribute('inputmode') == 'email') {
-    if ((fieldValue.length < 6) || (fieldValue.includes('.') == false) || (fieldValue.includes('@') == false)){
+    if ((fieldValue.length >= 6) || (fieldValue.includes('.')) || (fieldValue.includes('@'))){        
+        return true;
+    } else {
         invalidateField(valThisField,errorContainer,`${label} is not valid. Example: name@example.com.`);
         return false;
     }
