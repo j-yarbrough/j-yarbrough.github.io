3b94247430156ea1528fe76c9fe3d01827b22b04
Justin Yarbrough <80535881+j-yarbrough@users.noreply.github.com>
2026-02-15 11:08:54 -0600

    remove toc functionality

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -3,15 +3,11 @@ import markdownItAttrs  from "markdown-it-attrs";
 import pluginWebc from "@11ty/eleventy-plugin-webc";
 import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
 import { EleventyRenderPlugin } from "@11ty/eleventy";
-import { IdAttributePlugin } from "@11ty/eleventy";
 export default function(eleventyConfig) {
     eleventyConfig.setInputDirectory("_src");
 	eleventyConfig.addPlugin(pluginWebc);
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
     eleventyConfig.addPlugin(EleventyRenderPlugin);
-    eleventyConfig.addPlugin(IdAttributePlugin, {
-        selector: "h2,h3,h4,h5,h6"
-    });
     let options = {
 		html: true,
 		breaks: true,
