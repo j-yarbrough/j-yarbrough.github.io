b304f8eff3739f63c9289719d28f2e7874a10df4
j-yarbrough <j-yarbrough@users.noreply.github.com>
2026-03-16 16:39:55 +0000

    deploy: 2059e3e06aced2ebf03bdb830c866681f187b484

--- bundle/4Kb8FzzWFa.js
+++ bundle/4Kb8FzzWFa.js
@@ -146,3 +146,21 @@ window.addEventListener('load', function () {
     new TabsAutomatic(tablists[i]);
   }
 });
+window.addEventListener('load',function() {
+    document.querySelectorAll('.arrow-wrapper  button').forEach((arrowButton) => {
+        arrowButton.addEventListener('click',arrowButtonTrigger);
+    });
+});
+function arrowButtonTrigger() {
+var panelIndex = this.getAttribute('data-index');
+var tabToClick;
+var headingToFocus;
+switch(this.getAttribute('aria-label').includes('Previous')) {
+    case true: panelIndex--; break;
+    case false: panelIndex++; break;
+}
+tabToClick = document.getElementById(`tab-${panelIndex}`);
+headingToFocus = document.getElementById(`tabpanel-heading-${panelIndex}`);
+tabToClick.click();
+headingToFocus.focus()
+}
