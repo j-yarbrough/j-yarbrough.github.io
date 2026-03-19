5d702cf283a2dc36b38db4f634fb1cfc54d22be1
j-yarbrough <j-yarbrough@users.noreply.github.com>
2026-03-19 15:32:53 +0000

    deploy: 4082929ddcfcd2201b74b452e9a41e68d97ab0d8

--- bundle/DpI2EiVUbk.js
+++ bundle/DpI2EiVUbk.js
@@ -10,6 +10,26 @@ function triggerAccordion()  {
         case false: this.ariaExpanded = 'true'; break;
     }
 }
+window.addEventListener('load',function () {
+    this.document.querySelectorAll('tab-arrows button').forEach((arrowButton) => {
+        arrowButton.addEventListener('click',arrowButtonTrigger);
+    })
+})
+function arrowButtonTrigger () {
+    var tabInterface = this.closest('tab-interface');
+    var tabList = tabInterface.querySelector('[role="tablist"]');
+    var numberOfTabs = tabList.querySelectorAll('[role="tab"]').length;
+    var currentTabIndex = tabList.querySelector('[role="tab"][aria-selected="true"]').getAttribute('id').replace('tab-','');
+    var tabIndexToClick = currentTabIndex;
+    switch (this.getAttribute('aria-label')) {
+        case 'Previous tab':  tabIndexToClick--; break;
+        case 'Next tab': tabIndexToClick++; break;
+    }
+    if (tabIndexToClick == 0) tabIndexToClick = numberOfTabs;
+    if (tabIndexToClick > numberOfTabs) tabIndexToClick = 1;
+    tabList.querySelector(`#tab-${tabIndexToClick}`).click();
+    tabInterface.querySelector(`#tabpanel-heading-${tabIndexToClick}`).focus();
+}
 /*
  *   This content is licensed according to the W3C Software License at
  *   https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
