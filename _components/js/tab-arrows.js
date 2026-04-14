fa8c15906621aef62131617458ab41dc1a319cd5
Justin Yarbrough <80535881+j-yarbrough@users.noreply.github.com>
2026-04-14 13:27:50 -0500

    strip out some JS

--- _components/js/tab-arrows.js
+++ _components/js/tab-arrows.js
@@ -7,14 +7,13 @@ function arrowButtonTrigger () {
     var tabInterface = this.closest('tab-interface');
     var allTabs = tabInterface.querySelectorAll('[role="tab"]');
     var tabIndexToClick = tabInterface.querySelector('[role="tab"][aria-selected="true"]').getAttribute('id').replace('tab-','');
-    var tabToClick;
+
     switch (this.getAttribute('data-pn')) {
         case 'p':  tabIndexToClick--; break;
         case 'n': tabIndexToClick++; break;
     }
     if (tabIndexToClick == 0) tabIndexToClick = allTabs.length;
     if (tabIndexToClick > allTabs.length) tabIndexToClick = 1;
-    tabToClick = tabInterface.querySelector(`#tab-${tabIndexToClick}`);
-    tabToClick.click();
+    tabInterface.querySelector(`#tab-${tabIndexToClick}`).click();
     tabInterface.querySelector(`#tabpanel-heading-${tabIndexToClick}`).focus();
 }
