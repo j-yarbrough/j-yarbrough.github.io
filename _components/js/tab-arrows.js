dddc5660d70b2a2cab5f10a91bd445371428710f
Justin Yarbrough <80535881+j-yarbrough@users.noreply.github.com>
2026-04-14 13:20:48 -0500

    change to arrow script

--- _components/js/tab-arrows.js
+++ _components/js/tab-arrows.js
@@ -1,19 +1,20 @@
 window.addEventListener('load',function () {
-    this.document.querySelectorAll('tab-arrows button').forEach((arrowButton) => {
+    document.querySelectorAll('tab-arrows button').forEach((arrowButton) => {
         arrowButton.addEventListener('click',arrowButtonTrigger);
     })
 })
 function arrowButtonTrigger () {
     var tabInterface = this.closest('tab-interface');
-    var numberOfTabs = tabInterface.querySelectorAll('[role="tab"]').length;
-    var currentTabIndex = tabInterface.querySelector('[role="tab"][aria-selected="true"]').getAttribute('id').replace('tab-','');
-    var tabIndexToClick = currentTabIndex;
+    var allTabs = tabInterface.querySelectorAll('[role="tab"]');
+    var tabIndexToClick = tabInterface.querySelector('[role="tab"][aria-selected="true"]').getAttribute('id').replace('tab-','');
+    var tabToClick;
     switch (this.getAttribute('data-pn')) {
         case 'p':  tabIndexToClick--; break;
         case 'n': tabIndexToClick++; break;
     }
-    if (tabIndexToClick == 0) tabIndexToClick = numberOfTabs;
-    if (tabIndexToClick > numberOfTabs) tabIndexToClick = 1;
-    tabInterface.querySelector(`#tab-${tabIndexToClick}`).click();
+    if (tabIndexToClick == 0) tabIndexToClick = allTabs.length;
+    if (tabIndexToClick > allTabs.length) tabIndexToClick = 1;
+    tabToClick = tabInterface.querySelector(`#tab-${tabIndexToClick}`);
+    tabToClick.click();
     tabInterface.querySelector(`#tabpanel-heading-${tabIndexToClick}`).focus();
 }
