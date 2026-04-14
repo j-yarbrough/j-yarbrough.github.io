69f611c2b7329959b00c7f36796aa6e2def52f1a
j-yarbrough <j-yarbrough@users.noreply.github.com>
2026-04-14 18:29:59 +0000

    deploy: 4c3802b7ffc3ca31ecddf449443c301ca9ac5bc1

--- bundle/JiuJpjTX1Y.js
+++ bundle/JiuJpjTX1Y.js
@@ -1,15 +1,3 @@
-window.addEventListener('load',function () {
-    document.querySelectorAll('.modal-btn').forEach((dialogButton) => {
-        dialogButton.addEventListener('click',modalActivate);
-    })
-})
-function modalActivate() {
-    var dialogContainer = document.getElementById(this.getAttribute('aria-controls'));;
-    switch (this.hasAttribute('aria-haspopup')) {
-        case true:  dialogContainer.showModal();break;
-        case false: dialogContainer.close(); break;
-    }
-}
 window.addEventListener('load',function() {
     document.querySelectorAll('.acc-header [aria-expanded]').forEach((accButton) => {
         accButton.addEventListener('click',triggerAccordion);
@@ -23,21 +11,20 @@ function triggerAccordion()  {
     }
 }
 window.addEventListener('load',function () {
-    this.document.querySelectorAll('tab-arrows button').forEach((arrowButton) => {
+    document.querySelectorAll('tab-arrows button').forEach((arrowButton) => {
         arrowButton.addEventListener('click',arrowButtonTrigger);
     })
 })
 function arrowButtonTrigger () {
     var tabInterface = this.closest('tab-interface');
-    var numberOfTabs = tabInterface.querySelectorAll('[role="tab"]').length;
-    var currentTabIndex = tabInterface.querySelector('[role="tab"][aria-selected="true"]').getAttribute('id').replace('tab-','');
-    var tabIndexToClick = currentTabIndex;
+    var allTabs = tabInterface.querySelectorAll('[role="tab"]');
+    var tabIndexToClick = tabInterface.querySelector('[role="tab"][aria-selected="true"]').getAttribute('id').replace('tab-','');
     switch (this.getAttribute('data-pn')) {
         case 'p':  tabIndexToClick--; break;
         case 'n': tabIndexToClick++; break;
     }
-    if (tabIndexToClick == 0) tabIndexToClick = numberOfTabs;
-    if (tabIndexToClick > numberOfTabs) tabIndexToClick = 1;
+    if (tabIndexToClick == 0) tabIndexToClick = allTabs.length;
+    if (tabIndexToClick > allTabs.length) tabIndexToClick = 1;
     tabInterface.querySelector(`#tab-${tabIndexToClick}`).click();
     tabInterface.querySelector(`#tabpanel-heading-${tabIndexToClick}`).focus();
 }
