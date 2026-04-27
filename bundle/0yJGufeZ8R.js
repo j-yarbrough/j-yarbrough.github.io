0e75d570f0ce60327de1a08e929dc64caab5d3e5
j-yarbrough <j-yarbrough@users.noreply.github.com>
2026-04-27 12:27:11 +0000

    deploy: 3136de5872e2ac4adb42fc8ac5a4e83a57d8edc1

--- bundle/0yJGufeZ8R.js
+++ bundle/0yJGufeZ8R.js
@@ -23,14 +23,14 @@ function triggerAccordion()  {
     }
 }
 window.addEventListener('load',function () {
-    document.querySelectorAll('tab-arrows button').forEach((arrowButton) => {
+    document.querySelectorAll('tab-interface [data-pn]').forEach((arrowButton) => {
         arrowButton.addEventListener('click',arrowButtonTrigger);
     })
 })
 function arrowButtonTrigger () {
     var tabInterface = this.closest('tab-interface');
     var allTabs = tabInterface.querySelectorAll('[role="tab"]');
-    var tabIndexToClick = tabInterface.querySelector('[role="tab"][aria-selected="true"]').getAttribute('id').replace('tab-','');
+    var tabIndexToClick = tabInterface.querySelector('[role="tab"][aria-selected="true"]').getAttribute('id').replace('tab-',''); //gets currently selected tab, will be modified later
     switch (this.getAttribute('data-pn')) {
         case 'p':  tabIndexToClick--; break;
         case 'n': tabIndexToClick++; break;
