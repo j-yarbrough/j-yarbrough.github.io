2016031aed1bf383a31ab076caec70641933f5a3
Justin Yarbrough <80535881+j-yarbrough@users.noreply.github.com>
2026-05-03 07:33:54 -0500

    refactor aria live

--- _src/_includes/layouts/js/arialive.js
+++ _src/_includes/layouts/js/arialive.js
@@ -1,5 +1,5 @@
 function ariaLiveHandler (messageToAnnounce) {
-    var ariaRegion = document.querySelector('aria-live');
+    var ariaRegion = document.querySelector('#aria-live');
     ariaRegion.textContent = '';
     setTimeout(ariaRegion.textContent = messageToAnnounce, 200);
 }
